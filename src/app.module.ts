e43287788f977d894651f0b7f2c80cc12e39faa7
Temirlan <ismagulovt021@gmail.com>
2021-10-26 00:30:08 +0600

    add connection to postgres through typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  ENGINE,
+  ENGINE_HOST,
+  ENGINE_PORT,
+  ENGINE_USERNAME,
+  ENGINE_PASSWORD,
+  ENGINE_DB,
+} from './utils/constants';
+
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: ENGINE,
+      host: ENGINE_HOST,
+      port: ENGINE_PORT,
+      username: ENGINE_USERNAME,
+      password: ENGINE_PASSWORD,
+      database: ENGINE_DB,
+      entities: [],
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,9 @@ import { config } from 'dotenv';
 config();
 
 export const APP_PORT = process.env.APP_PORT;
+export const ENGINE = process.env.ENGINE as 'cockroachdb';
+export const ENGINE_HOST = process.env.ENGINE_HOST as string;
+export const ENGINE_PORT = +process.env.ENGINE_PORT as number;
+export const ENGINE_USERNAME = process.env.ENGINE_USERNAME as string;
+export const ENGINE_PASSWORD = process.env.ENGINE_PASSWORD as string;
+export const ENGINE_DB = process.env.ENGINE_DB as string;
